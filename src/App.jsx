731d00504437230a6e30b95677051c17387fb17c
aditya040604 <adityagurram04@gmail.com>
2025-04-28 18:03:34 +0530

    adding my first react project to github

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react"
+
+
+function Agenda({onSubmit, onChange, taskList, task, editIndex, handleOnEdit, setEditIndex, onEditChange, setEditTask, editTask}){
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="addTask"  onChange={onChange} value={task}/>
+        <button type="submit">Add</button>
+
+      </form>
+      <div>
+        <ul>
+
+        {taskList.map((task, index) => (
+          editIndex !== index ? <li key={index}>{task} <button onClick={(e) => [setEditIndex(index),setEditTask(task)]}>edit</button> <button>delete</button></li> :(
+            <>
+            <form onSubmit={(e) => handleOnEdit(e,index)}>
+            <input type="text" name="editTask" onChange={onEditChange} key={index} value={editTask}/>
+            <button type="submit">submit</button>
+            </form>
+            
+            </>
+            
+            
+          ) 
+          
+
+          
+        ))}
+        </ul>
+        
+
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+function App() {
+  const [task, setTask] = useState('')
+  const [taskList, setTaskList] = useState([])
+  const [editIndex, setEditIndex] = useState(null)
+  const [editTask, setEditTask] = useState(null)
+ 
+
+  function onEditChange(e) {
+    let value = e.target.value
+    setEditTask(value)
+  }
+
+
+  function handleOnEdit(e, index){
+    e.preventDefault()
+   
+      const newTaskList = structuredClone(taskList)
+      newTaskList[Number(index)] = editTask
+      setTaskList(newTaskList)
+      setEditIndex(null)
+      setEditTask(null)
+
+    
+  }
+
+  function handleOnChange(e){
+    let value = e.target.value
+    setTask(value)
+  }
+  function handleOnSubmit(e){
+    e.preventDefault()
+    
+    if (task !== ''){
+      setTaskList(prev => ([
+        ...prev, task
+      ]))
+      setTask('')
+    }
+  }
+  return (
+   <>
+  <h1>
+    Agenda
+  </h1>
+    <Agenda onChange={handleOnChange} onSubmit={handleOnSubmit} taskList={taskList} task={task} editIndex={editIndex} handleOnEdit={handleOnEdit} setEditIndex={setEditIndex} onEditChange={onEditChange} setEditTask={setEditTask} editTask={editTask}/>
+   
+   
+   
+   </>
+  )
+}
+
+export default App

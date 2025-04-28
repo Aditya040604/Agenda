fc051bec33ad5b5cbcc87011be8909f64e601861
aditya040604 <adityagurram04@gmail.com>
2025-04-28 18:37:53 +0530

    updating existing file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { useEffect } from "react"
+import { useRef } from "react"
 import { useState } from "react"
 
 
-function Agenda({onSubmit, onChange, taskList, task, editIndex, handleOnEdit, setEditIndex, onEditChange, setEditTask, editTask}){
+function Agenda({onSubmit, onChange, taskList, task, editIndex, handleOnEdit, setEditIndex, onEditChange, setEditTask, editTask , editInputRef}){
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -13,11 +15,12 @@ function Agenda({onSubmit, onChange, taskList, task, editIndex, handleOnEdit, se
         <ul>
 
         {taskList.map((task, index) => (
-          editIndex !== index ? <li key={index}>{task} <button onClick={(e) => [setEditIndex(index),setEditTask(task)]}>edit</button> <button>delete</button></li> :(
+          editIndex !== index ? <li key={index}>{task} <button onClick={(e) => {setEditIndex(index),setEditTask(task)
+          }}>edit</button> <button>delete</button></li> :(
             <>
             <form onSubmit={(e) => handleOnEdit(e,index)}>
-            <input type="text" name="editTask" onChange={onEditChange} key={index} value={editTask}/>
-            <button type="submit">submit</button>
+            <input type="text" name="editTask" onChange={onEditChange} key={index} value={editTask} onBlur={(e) => handleOnEdit(e, index)} ref={editInputRef}/>
+            <button type="submit" disabled={!editTask}>submit</button>
             </form>
             
             </>
@@ -45,6 +48,7 @@ function App() {
   const [taskList, setTaskList] = useState([])
   const [editIndex, setEditIndex] = useState(null)
   const [editTask, setEditTask] = useState(null)
+  const editInputRef = useRef(null)
  
 
   function onEditChange(e) {
@@ -55,12 +59,14 @@ function App() {
 
   function handleOnEdit(e, index){
     e.preventDefault()
-   
+    if (index != null && editTask.trim() !== ''){
       const newTaskList = structuredClone(taskList)
       newTaskList[Number(index)] = editTask
       setTaskList(newTaskList)
       setEditIndex(null)
       setEditTask(null)
+    }
+     
 
     
   }
@@ -79,12 +85,18 @@ function App() {
       setTask('')
     }
   }
+
+  useEffect(() => {
+    if (editIndex !== null){
+      editInputRef.current?.focus()
+    }
+  }, [editIndex])
   return (
    <>
   <h1>
     Agenda
   </h1>
-    <Agenda onChange={handleOnChange} onSubmit={handleOnSubmit} taskList={taskList} task={task} editIndex={editIndex} handleOnEdit={handleOnEdit} setEditIndex={setEditIndex} onEditChange={onEditChange} setEditTask={setEditTask} editTask={editTask}/>
+    <Agenda onChange={handleOnChange} onSubmit={handleOnSubmit} taskList={taskList} task={task} editIndex={editIndex} handleOnEdit={handleOnEdit} setEditIndex={setEditIndex} onEditChange={onEditChange} setEditTask={setEditTask} editTask={editTask} editInputRef={editInputRef}/>
    
    
    
